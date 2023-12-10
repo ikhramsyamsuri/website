b1745606f1785670ecab045f4428e9a3e924ffee
Muhammad Ikhram Syamsuri <muhammadikhram3@gmail.com>
2023-12-10 11:14:05 +0700

    Basic Django CRUD

---
+++ websitetesting/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from .views import ItemList, ItemDetail, LocationList, LocationDetail
+
+urlpatterns = [
+    path('item/', ItemList.as_view()),
+    path('item/<int:pk>/', ItemDetail.as_view()),
+    path('Location/', LocationList.as_view()),
+    path('Location/<int:pk>/', LocationDetail.as_view()),
+]
